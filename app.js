e15ba0f1284baaa1b898639ca685e95f06767676
Mostafa <m.elsheikh11791@gmail.com>
2016-11-23 16:36:54 +0200

    fixed logic for checking items off

--- app.js
+++ app.js
@@ -25,40 +25,56 @@ var itemTemplate = (
 function addItem(item) {
 	state.items.push({
 		displayName: item,
+        checked: false
 	});
 };
+
+function toggleItemStatus(index) {
+    state.items[index].checked = !state.items[index].checked;
+}
 
 function renderList() {
 	var itemsHtml = [];
 	for (var i = 0; i < state.items.length; i++) {
-		itemsHtml.push(renderItem(state.items[i]));
+		itemsHtml.push(renderItem(state.items[i], i));
 		console.log(itemsHtml);
 	}
 	$('.shopping-list').html(itemsHtml);
 }
 
-function renderItem(stateItem) {
+function renderItem(stateItem, index) {
 	var item = $(itemTemplate);
 	var displayName = stateItem.displayName;
 	item.find('.shopping-item').text(displayName);
+    item.attr('id', index);
+    // check if an item is checked, in which case add class to show visual check
+    if (state.items[index].checked == true) {
+        item.addClass('shopping-item__checked');
+    }
 	return item;
 }
 
+// document.ready
 $(function() {
+    registerItemCheckHandler();
+
 	$('#js-shopping-list-form').submit(function(event) {
     	event.preventDefault();
-    	registerItemCheckHandler();
     	addItem($('#shopping-list-entry').val());
     	renderList();
-    	
 	});
-
 });
 
 function registerItemCheckHandler() {
+    // listener for full list element
 	$('ul').on('click', '.js-shopping-item-toggle', function(event) {
-		console.log('click');
-    	$(this).closest('li').addClass('shopping-item__checked');
+        // clicked list items
+        var li = $(this).closest('li');
+        // add class to make item visually checked
+    	li.addClass('shopping-item__checked');
+        // logic to update our in memory state to reflect that item has been checked
+        var liId = li.attr('id');
+        toggleItemStatus(liId);
 	});
 
 }
