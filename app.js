65f9fbdb0ad6eb08fe6194a67a0c04ffa25545f6
Laura Batson <laurapetitt@gmail.com>
2016-11-22 20:39:09 -0500

    Add files via upload

--- app.js
+++ app.js
@@ -1,7 +1,12 @@
+//after spending hours and hours just trying to understand the Thinkful solution to this, I'm moving on. James Duncan says
+//what I've got is good enough.
+
+//create an empty array for all added items
 var state = {
     items: []
 };
 
+//add all html to the items
 var itemTemplate = (
 	'<li>' +
     '<span class="shopping-item js-shopping-item"></span>' +
@@ -16,11 +21,10 @@ var itemTemplate = (
   '</li>'
 );
 
-
+//function for each item added
 function addItem(item) {
 	state.items.push({
 		displayName: item,
-		checkedOff: false
 	});
 };
 
@@ -28,6 +32,7 @@ function renderList() {
 	var itemsHtml = [];
 	for (var i = 0; i < state.items.length; i++) {
 		itemsHtml.push(renderItem(state.items[i]));
+		console.log(itemsHtml);
 	}
 	$('.shopping-list').html(itemsHtml);
 }
@@ -42,22 +47,18 @@ function renderItem(stateItem) {
 $(function() {
 	$('#js-shopping-list-form').submit(function(event) {
     	event.preventDefault();
+    	registerItemCheckHandler();
     	addItem($('#shopping-list-entry').val());
     	renderList();
-        // need to call this function every time an item is added to the DOM
-        registerItemCheckHandler();
+    	
 	});
+
 });
 
-// function to register a click handler function for all elements with a 'js-shopping-item-toggle'
-// class. Everytime an element is added to the DOM, we must re-register.
-// The problem initially was that when the code below ran, no elements were in the DOM. We need to re-run
-// this code everytime we add an element to the DOM.
 function registerItemCheckHandler() {
-    $('.js-shopping-item-toggle').click(function(event) {
-        // This will check all elements in the list as they all have the class.
-    	//$('.js-shopping-item').closest('li').addClass('shopping-item__checked');
-        // $(this) is the clicked one, so we get it's closest li element and add our class to it.
-        $(this).closest('li').addClass('shopping-item__checked');
+	$('ul').on('click', '.js-shopping-item-toggle', function(event) {
+		console.log('click');
+    	$(this).closest('li').addClass('shopping-item__checked');
 	});
+
 }
